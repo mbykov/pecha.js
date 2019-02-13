0bf057b4d2e36e5ede4c284fb7291255af8e2346
Michael Bykov <m.bykov@gmail.com>
2019-02-13 16:54:37 +0300

    i

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,6 +1,6 @@
 //
 import _ from 'lodash'
-import { parsePDCHs, totalKeys } from "./segmenter";
+import { parsePDCHs, scrape, segmenter, totalKeys } from "./segmenter";
 import { getPossible } from "./pouch";
 import { tibsyms, tibsyls } from "./tibetan_data";
 import { showResults, replaceTarget } from "./parsedata";
@@ -10,22 +10,26 @@ const log = console.log
 
 export function mainResults(el) {
   let str = el.textContent
-  let syls = _.compact(str.split(tibsyms.tsek))
-  let pdchs = parsePDCHs(syls)
+  // let syls = _.compact(str.split(tibsyms.tsek))
+  // let pdchs = parsePDCHs(syls)
+  // let flakes = scrape(str)
+  // log('MAIN flakes:', flakes)
+
+  let pdchs = segmenter(str)
+  log('MAIN pdchs:', pdchs)
+  // return
+
   let keys = totalKeys(pdchs)
-  log('MAIN pdchs:', pdchs, keys.length)
+  log('MAIN keys:', keys.length)
   getPossible(keys)
     .then(docs=> {
       docs = _.flatten(docs)
-      // log('DOCS', docs)
+      log('DOCS', docs)
       let chains = makeChains(pdchs, docs)
-      // log('CHs', chains)
       let fulls = fullChains(chains)
       log('chains: ', chains.length, 'fulls: ', fulls.length)
       if (fulls.length) chains = fulls
-      // log('CHs', chains)
-      // let trns = chains.map(segs=> { return segs.map(seg=> { return [seg.seg, seg.docs.map(doc=> { return doc.trns })].join('-') })})
-      // log('TRNS', trns.toString())
+      log('CHs', chains)
       let bests = selectLongest(chains)
       log('bests =>', bests.length, bests)
       let best = bests[0]
@@ -50,6 +54,18 @@ function makeChains(pdchs, docs) {
   return chains
 }
 
+function selectLongest(chains) {
+  let max = _.max(chains.map(chain => {  return _.sum(chain.map(segment => { return segment.docs.length ? segment.seg.length : 0 }))/chain.length } ) )
+  log('MAX', max)
+  let longests = _.filter(chains, chain => { return _.sum(chain.map(segment => { return segment.docs.length ? segment.seg.length : 0 }))/chain.length >= max - 1 })
+  longests = _.sortBy(longests, chain => { return _.sum(chain.map(segment => { return segment.docs.length ? segment.seg.length : 0 }))/chain.length }).reverse()
+  log('LNGST', longests)
+  let min = _.min(longests.map(chain => {  return chain.length } ) )
+  log('MIN', min)
+  let shortests = _.filter(longests, chain => { return chain.length == min })
+  return shortests
+}
+
 function startWith(str, head) {
   if (str == head) return true
   let reh = new RegExp('^' + head)
@@ -68,15 +84,4 @@ function fullChains(chains) {
     if (full) fulls.push(segs)
   })
   return fulls
-}
-
-function selectLongest(chains) {
-  let max = _.max(chains.map(chain => {  return _.sum(chain.map(segment => { return segment.seg.length }))/chain.length } ) )
-  log('MAX', max)
-  let longests = _.filter(chains, chain => { return _.sum(chain.map(segment => { return segment.seg.length }))/chain.length >= max -1 })
-  longests = _.sortBy(longests, chain => { return _.sum(chain.map(segment => { return segment.seg.length }))/chain.length }).reverse()
-  let min = _.min(longests.map(chain => {  return chain.length } ) )
-  log('MIN', min)
-  let shortests = _.filter(longests, chain => { return chain.length == min })
-  return shortests
 }
--- src/lib/segmenter.js
+++ src/lib/segmenter.js
@@ -3,20 +3,7 @@ import { getPossible } from "./pouch";
 import { tibsyms, tibsyls } from "./tibetan_data";
 
 const log = console.log
-
-export function totalKeys(pdchs) {
-  let segs = _.flatten(pdchs)
-  let added = []
-  segs.forEach(seg=>{
-    tibsyls.forEach(syl=>{
-      let resyl = new RegExp(syl+'$')
-      let poss = seg.replace(resyl, '')
-      if (seg != poss) added.push(poss)
-    })
-  })
-  let keys = segs.concat(added)
-  return _.uniq(keys)
-}
+const tsek = tibsyms.tsek
 
 export function parsePDCHs(segs) {
   let h, t
@@ -36,18 +23,68 @@ export function parsePDCHs(segs) {
   return pdchs
 }
 
-function parseKeys_(segs) {
-  let h, t
-  let padas = []
-  for (let idx = 1; idx < segs.length + 1; idx++) {
-    h = segs.slice(0, idx).join(tibsyms.tsek)
-    t = segs.slice(idx)
-    padas.push(h)
-    let h_
-    for (let idy = 1; idy < t.length + 1; idy++) {
-      h_ = t.slice(0, idy).join(tibsyms.tsek)
-      padas.push(h_)
-    }
+function scrape_ (str) {
+  let total = str.length+1
+  let flakes = []
+  let head = str
+  let pos = str.length
+  let tail
+  while (pos > 0) {
+    pos--
+    head = str.substr(0, pos)
+    if (!head) continue
+    tail = str.slice(pos)
+    let res = {head: head, tail: tail}
+    flakes.push(res)
   }
-  return padas
+  return flakes
+}
+export function totalKeys(pdchs) {
+  let segs = _.uniq(_.flatten(pdchs))
+  let added = []
+  segs.forEach(seg=>{
+    tibsyls.forEach(syl=>{
+      let resyl = new RegExp(syl+'$')
+      let poss = seg.replace(resyl, '')
+      if (seg != poss) added.push(poss)
+    })
+  })
+  let keys = segs.concat(added)
+  return _.uniq(keys)
+}
+
+export function segmenter(str) {
+  let old = str
+  let pdchs = []
+  function rec(str, pdch) {
+    // log('PDCH', pdch)
+    let flakes = scrape(str)
+    flakes.forEach(flake => {
+      if (!flake.tail) return
+      pdch.push(flake.head)
+      pdch.push(flake.tail)
+      if (pdch.join(tsek) == old) {
+        pdchs.push(_.clone(pdch))
+        pdch.pop()
+      }
+      if (pdch.length < 10) rec(flake.tail, pdch) // three parts for now !
+      rec(flake.tail, pdch)
+      pdch.pop()
+    })
+  }
+  rec(str, [])
+  return pdchs
+}
+
+export function scrape(str) {
+  let segs = str.split(tsek)
+  let head, tail
+  let flakes = []
+  for (let idx = 1; idx < segs.length + 1; idx++) {
+    head = segs.slice(0, idx).join(tsek)
+    tail = segs.slice(idx).join(tsek)
+    let flake = {head: head, tail: tail}
+    flakes.push(flake)
+  }
+  return flakes.reverse()
 }
