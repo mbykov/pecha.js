217f576bbccc9af572a74e6fef6f0ef1adff484f
Michael Bykov <m.bykov@gmail.com>
2019-03-22 19:55:08 +0300

    v.0.8.25

--- src/dbs/glob2csv.js
+++ src/dbs/glob2csv.js
@@ -12,7 +12,7 @@ import sband from "speckled-band"
 const stream = require("stream")
 const gs = require('glob-stream');
 const MultiStream = require('multistream')
-import { queryDBs } from "../pouch"
+import { queryDBs } from "./pouch"
 
 let code  = 'tib'
 import { tibsyms, tibsuff } from "../lib/tibetan_data"
@@ -27,8 +27,8 @@ export function glob2csv(globpath) {
   const toTibetan = miss.through.obj((data, enc, cb) => {
     selectTib(data.path)
       .then(function(wfs) {
-        log('d', data.path)
-        log('_wfs_', wfs.length)
+        // log('d', data.path)
+        // log('_wfs_', wfs.length)
         wfs = wfs.slice(0,3)
         if (wfs.length) cb(null, wfs);
         else cb()
@@ -37,20 +37,64 @@ export function glob2csv(globpath) {
       })
   })
 
+  let rs2 = stream.Readable({objectMode: true})
+  rs2._read = () => {}
+
+  function recQuery(query) {
+    function decide(res) {
+      if (!res.chain) return
+      res.chain.forEach(sec=> {
+        if (sec.docs.length) rs2.push(sec.seg) // log('______sec.seg', sec.seg)// saveChunk(sec.seg) //dicts.push(sec.seg)
+        else {
+          if (query.str != sec.seg) return recQuery({ str: sec.seg })
+        }
+      })
+    }
+    return queryDBs(query).then(decide);
+  }
+
   const toDict = miss.through.obj((wfs, enc, next) => {
+    let idx = 0
+    wfs.forEach(wf=> {
+      let query = {str: wf}
+      recQuery(query)
+        .catch(function(err) {
+          console.log('Q-ERR', err)
+        })
+    })
+    next()
+  })
+
+  const toDict_ = miss.through.obj((wfs, enc, next) => {
     let idx = 0
     function factory (cb) {
       wfs.forEach(wf=> {
         log('____wf', wf)
-        setTimeout(function () {
-          let rs = stream.Readable({objectMode: true})
-          rs._read = () => {}
-          rs.push(wf)
-          if (idx == 0)
-            idx++, factory (cb)
-          else
-            cb(null, rs)
-        }, 100)
+        let query = {str: wf}
+
+        queryDBs(query)
+          .then(function(res) {
+            // log('____res', res)
+            if (!res.chain) return cb()
+            let rs = stream.Readable({objectMode: true})
+            rs._read = () => {}
+            res.chain.forEach(sec=> {
+              if (sec.docs.length) rs.push(sec.seg), cb(null, rs)
+              else {
+                // if (query.str != sec.seg) return factory(cb)
+              }
+            })
+          })
+
+        // setTimeout(function () {
+        //   let rs = stream.Readable({objectMode: true})
+        //   rs._read = () => {}
+        //   rs.push(wf)
+        //   if (idx == 0)
+        //     idx++, factory (cb)
+        //   else
+        //     cb(null, rs)
+        // }, 100)
       })
     }
     // MultiStream(factory).pipe(process.stdout)
@@ -59,15 +103,16 @@ export function glob2csv(globpath) {
   })
 
   const toStr = miss.through.obj((data, enc, cb) => {
-    let json = JSON.stringify(data) + '\n'
-    // let str = data.toString() + '\n'
+    // let json = JSON.stringify(data) + '\n'
+    let str = data.toString() + '\n'
     // log('json:', json)
-    cb(null, json);
+    cb(null, str);
   })
 
   // rs.pipe(toStr).pipe(process.stdout);
-  rs.pipe(toTibetan).pipe(toStr).pipe(process.stdout);
-  // rs.pipe(toTibetan).pipe(toDict)
+  // rs.pipe(toTibetan).pipe(toStr).pipe(process.stdout);
+  rs.pipe(toTibetan).pipe(toDict)
+  rs2.pipe(toStr).pipe(process.stdout);
 }
 
 function selectTib(fpath) {
